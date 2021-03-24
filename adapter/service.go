b519e6802fbd5ca3401b6aeb2d3379d0486c08e6
Theo Sun <theo.sun@sap.com>
2021-03-24 13:05:16 +0800

    feat: update archive service

--- adapter/service.go
+++ adapter/service.go
@@ -2,7 +2,17 @@ package adapter
 
 import "github.com/ArchiveLife/model/model"
 
+// ArticleConsumer function, make client could consume large stream data
+type ArticleConsumer func(*model.Article)
+
+// ArchiveService
 type ArchiveService interface {
+	// get the name of service
+	GetName() string
+	// get the description of service
+	GetDescription() string
+	// get the options of service
 	GetOptions() []*Option
-	Run(...OptionValue) []*model.Article
+	// run service with input options
+	Run(ArticleConsumer, ...OptionValue)
 }
